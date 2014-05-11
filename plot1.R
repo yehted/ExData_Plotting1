d36e9db9e3d28a89af85919033eec88796125fe8
yehted <yeh.ted@gmail.com>
2014-05-11 16:31:45 -0500

    plots 1 and 2

---
+++ plot1.R
@@ -0,0 +1,21 @@
+plot1 <- function(directory, filename) {
+  
+  file <- paste(directory, "/", filename, ".txt", sep ="")
+  sampleData <- read.csv(file, sep=";", nrows = 3)
+  classes <- sapply(sampleData, class)
+  classes[1:2] <- "character"
+  power <- read.table(file, sep=";", colClasses = classes, na.strings = "?", header = TRUE)
+  power$Time <- strptime(paste(power$Date, power$Time, sep = " "), format = "%d/%m/%Y %H:%M:%S")
+  power$Date <- as.Date(power$Date, "%d/%m/%Y")
+  
+  dates <- c("2007-02-01", "2007-02-02")
+  dates <- as.Date(dates, "%Y-%m-%d")
+  
+  data <- power[power$Date %in% dates,]
+  
+  hist(data$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)")
+  windows()
+  plot(data$Time, data$Global_active_power, type = "l", col = "black", xlab="", ylab = "Global Active Power (kilowatts)")
+  windows()
+  
+}  
